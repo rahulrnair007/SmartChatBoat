f8ac12fcc642c361adcb4dd6fafaff735ef31775
Rahul R Nair <100027367+rahulrnair007@users.noreply.github.com>
2022-04-04 13:58:46 +0530

    SmartChatBoat server side .net core project added

---
+++ API/Covid_SmartChatBot/Covid_SmartChatBot/UserData.cs
@@ -0,0 +1,24 @@
+﻿namespace Covid_SmartChatBot
+{
+    /// <summary>
+    /// Respresents Model UserData.
+    /// </summary>
+    public class UserData
+    {
+        /// <summary>
+        /// Gets or sets the query data.
+        /// </summary>
+        /// <value>
+        /// The query data.
+        /// </value>
+        public string queryData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the session identifier.
+        /// </summary>
+        /// <value>
+        /// The session identifier.
+        /// </value>
+        public string  sessionId { get; set; }
+    }
+}
